13b94d75bf269a46da2af0f0ee713a0dc2a3b2fa
Egor Kolesnikov <freethinkel@gmail.com>
2024-01-30 19:22:08 +0600

    fix tiktok loading

--- modules/tiktok/index.ts
+++ modules/tiktok/index.ts
@@ -1,32 +1,27 @@
-import { Middleware } from 'grammy';
-import { DOMParser } from 'deno:dom';
-import { initEnv } from '../helpers/index.ts';
+import { Middleware } from "grammy";
+import { initEnv } from "../helpers/index.ts";
 
 await initEnv();
 
 export class TiktokModule {
-	constructor(private url: string) {}
+  constructor(private url: string) {}
 
-	async getVideoUrl(): Promise<string> {
-		const html = await fetch(this.url).then((res) => res.text());
-		const document = new DOMParser().parseFromString(html, 'text/html');
-		const json = JSON.parse(
-			document?.querySelector('script[id="SIGI_STATE"]')?.innerText || '{}'
-		);
+  async getVideoUrl(): Promise<string> {
+    const res = await fetch(`https://tiktod.eu.org/download?url=${this.url}`);
+    const data = await res.json();
+    return data.result.media;
+  }
 
-		return json.ItemModule[json.ItemList.video.keyword].video.downloadAddr;
-	}
+  static register: Middleware = async (ctx) => {
+    try {
+      const text = ctx.message?.text || "";
 
-	static register: Middleware = async (ctx) => {
-		try {
-			const text = ctx.message?.text || '';
+      const tm = new this(text);
+      const url = await tm.getVideoUrl();
 
-			const tm = new this(text);
-			const url = await tm.getVideoUrl();
-
-			ctx.replyWithVideo(url);
-		} catch (_) {
-			ctx.reply('произошла ошибка при загрузке видео :(');
-		}
-	};
+      await ctx.replyWithVideo(url);
+    } catch (_) {
+      ctx.reply("произошла ошибка при загрузке видео :(");
+    }
+  };
 }
